159a5e960f8ae59bef22b3bbe0caa84cb2a98a48
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-12-23 12:34:48 +0100

    increase timeout of failing test

--- test/builder/runtime.spec.ts
+++ test/builder/runtime.spec.ts
@@ -3,11 +3,12 @@ import {dirname} from "path";
 import * as assert from "assert";
 import Builder from "../../src/builder/Builder";
 
-it('parses mem.cots',  () => {
+it('parses mem.cots',  (done) => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(dirname(dirname(__filename)));
     const path = __dirname + "/src/runtime/mem.cots";
     const unit = Builder.parse_unit(path);
     assert.equal(unit.declarations.length, 27);
     assert.equal(unit.statements.length, 15);
- });
+    done();
+ }).timeout(5000);
