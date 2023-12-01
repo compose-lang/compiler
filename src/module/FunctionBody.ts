710ff2d568ddf33a2762579686d6150e49a4ae51
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2023-12-01 13:57:42 +0100

    more binaryen apis

--- src/module/FunctionBody.ts
+++ src/module/FunctionBody.ts
@@ -2,6 +2,10 @@ import IType from "../type/IType";
 import IValueType from "../type/IValueType";
 import binaryen from "binaryen";
 import Fragment from "../builder/Fragment";
+import ExpressionRef = binaryen.ExpressionRef;
+import FunctionRef = binaryen.FunctionRef;
+import Module = binaryen.Module;
+import Type = binaryen.Type;
 
 enum LocalScope {
     PARAM,
@@ -18,7 +22,21 @@ interface Local {
 
 /* TODO remove once binaryen.js is upgraded */
 type IFunction = {
-    setLocalName(func: binaryen.ExpressionRef, index: number, name: string): void;
+    // @ts-ignore
+    getName(func: FunctionRef): string;
+    getParams(func: FunctionRef): Type;
+    getResults(func: FunctionRef): Type;
+    getNumVars(func: FunctionRef): number;
+    getVar(func: FunctionRef, index: number): Type;
+    getNumLocals(func: FunctionRef): number;
+    hasLocalName(func: FunctionRef, index: number): boolean;
+    getLocalName(func: FunctionRef, index: number): string;
+    setLocalName(func: ExpressionRef, index: number, name: string): void;
+    getBody(func: FunctionRef): ExpressionRef;
+    setBody(func: FunctionRef, bodyExpr: ExpressionRef): void;
+    optimize(func: FunctionRef, module: Module): void;
+    runPasses(func: FunctionRef, module: Module, passes: string[], numPasses: number): void;
+    setDebugLocation(func: FunctionRef, expr: ExpressionRef, fileIndex: number, lineNumber: number, columnNumber: number): void;
 };
 
 export default class FunctionBody {
