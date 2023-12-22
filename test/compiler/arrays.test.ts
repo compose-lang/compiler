abbb02d20ac9a7c8970b50af06bd937f749cf40b
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2023-12-22 09:16:07 +0100

    fix failing test

--- test/compiler/arrays.test.ts
+++ test/compiler/arrays.test.ts
@@ -1,17 +1,14 @@
 import Pipeline from "../../src/compiler/Pipeline.ts";
 import ComposeBuilder from "../../src/builder/ComposeBuilder.ts";
 import Runner from "../../src/runner/Runner.ts";
-import { assertEquals } from "https://deno.land/std@0.209.0/assert/mod.ts";
-import PipelineOptions from "../../src/compiler/PipelineOptions.ts";
+import { assert } from "../../deps.ts";
 
-Deno.test('returns an i32 array',  () => {
-    const options = PipelineOptions.DEFAULTS
-        .with(options => options.dumpWatPath = "dumps/returns_an_i32_array.wat")
-        .with(options => options.dumpWasmPath = "dumps/returns_an_i32_array.wasm")
-    const pipeline = new Pipeline(options);
+
+Deno.test('returns an opaque i32 array',  () => {
+    const pipeline = new Pipeline();
     const unit = ComposeBuilder.parse_unit("@ModuleExport function stuff(): i32[] { const a:i32[] = [ 3, 5 ]; return a; }");
     const wasmTarget = pipeline.build([unit])[0];
     const runner = Runner.of(wasmTarget.asWasmSource());
     const result = runner.runFunction<number>("stuff");
-    // assertEquals(result, []);
+    assert(result);
 });
