0229dbab45d06e3168812528d62f86d196ce4b74
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2024-01-24 21:32:43 +0100

    parses String class

---
+++ test/builder/builtins.test.ts
@@ -0,0 +1,17 @@
+import ComposeBuilder from "../../src/builder/ComposeBuilder.ts";
+import {assertEquals, assertTrue} from "../../src/platform/deno/AssertUtils.ts";
+import ClassDeclaration from "../../src/declaration/ClassDeclaration.ts";
+import {fileURLToPath} from "../../src/platform/deno/URLUtils.ts";
+import {dirname} from "../../src/platform/deno/FileUtils.ts";
+import {FileStream} from "../../src/platform/deno/Antlr4FileStream.ts";
+
+Deno.test('parses String class',  () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(dirname(dirname(__filename)));
+    const unitPath = __dirname + "/runtime/rt/strings.cots";
+    const unit = ComposeBuilder.parse_unit_stream(new FileStream(unitPath));
+    assertTrue(unit);
+    assertEquals(unit.declarations.length, 1);
+    assertTrue(unit.declarations[0] instanceof ClassDeclaration);
+    assertEquals(unit.declarations[0].name, "String");
+});
